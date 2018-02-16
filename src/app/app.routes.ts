2fcb3f9bb48ca4dd26d302d6aae5385ba4c6a70c
Stephen Wright <sjw948@gmail.com>
2018-02-16 08:03:32 +0000

    routes for about / contact components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,15 @@
 import { RouterModule, Routes } from '@angular/router';
 
-import { HomeViewComponent } from 'app/modules/website/components/home/view/home-view.component';
 import { WebsiteViewWrapperComponent } from 'app/modules/website/components/view-wrapper/website-view-wrapper.component'
+import { HomeViewComponent } from 'app/modules/website/components/home/view/home-view.component';
+import { AboutViewComponent } from 'app/modules/website/components/about/view/about-view.component';
+import { ContactViewComponent } from 'app/modules/website/components/contact/view/contact-view.component';
 
 const APP_ROUTES: Routes = [
   {path: '', component: WebsiteViewWrapperComponent, children: [
-    {path: '', component: HomeViewComponent}
+    {path: '', component: HomeViewComponent},
+    {path: 'about', component: AboutViewComponent},
+    {path: 'contact', component: ContactViewComponent}
   ]},
 ]
 
--- src/app/modules/website/components/components-barrel.ts
+++ src/app/modules/website/components/components-barrel.ts
@@ -1,3 +1,5 @@
 export { WebsiteViewWrapperComponent } from 'app/modules/website/components/view-wrapper/website-view-wrapper.component';
 export { HomeViewComponent } from 'app/modules/website/components/home/view/home-view.component';
 export { WebsiteMenuComponent } from 'app/modules/website/components/menu/menu/website-menu.component';
+export { AboutViewComponent } from 'app/modules/website/components/about/view/about-view.component';
+export { ContactViewComponent } from 'app/modules/website/components/contact/view/contact-view.component';
