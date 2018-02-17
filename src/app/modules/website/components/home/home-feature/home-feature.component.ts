fa5d14c05c15a1e2aaf73eeb9feab21d4169f388
Stephen Wright <sjw948@gmail.com>
2018-02-17 14:39:53 +0000

    features / feature containers

--- src/app/modules/website/components/home/home-feature/home-feature.component.ts
+++ src/app/modules/website/components/home/home-feature/home-feature.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-home-feature',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-feature.component.scss']
 })
 export class HomeFeatureComponent implements OnInit {
+  @Input() feature: object;
 
   constructor() { }
 
--- src/app/modules/website/components/home/home-features/home-features.component.ts
+++ src/app/modules/website/components/home/home-features/home-features.component.ts
@@ -7,9 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeFeaturesComponent implements OnInit {
 
+  features: Array<object>
+
   constructor() { }
 
   ngOnInit() {
+    this.features = [
+      {
+        icon: 'fa fa-hand-rock fa-3x',
+        title: 'Martial Art',
+        text: `Using Martial Arts as the platform to teach more than just self defence.
+               Wanting to pass on the skills to combat lifes challenges and the tools
+               to achieve in the hardest arena of all… Life!`
+      },
+      {
+        icon: 'fa fa-smile fa-3x',
+        title: 'Correct Ethics',
+        text: 'I\'m some feature text 1'
+      },
+      {
+        icon: 'fa fa-comment fa-3x',
+        title: 'Kaizen',
+        text: 'I\'m some feature text 2'
+      },
+      {
+        icon: 'fa fa-paint-brush fa-3x',
+        title: 'Art',
+        text: 'I\'m some feature text 3'
+      },
+    ]
   }
+
 
 }
